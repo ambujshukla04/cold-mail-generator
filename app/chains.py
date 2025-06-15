513ae3fe3a5af09ed6e752409ca89f5f8d3bc910
AMBUJ SHUKLA <145540852+ambujshukla04@users.noreply.github.com>
2025-06-15 11:20:05 +0530

    Add files via upload

---
+++ app/chains.py
@@ -0,0 +1,61 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            You are Ambuj Shukla,I am a Computer Science student at PSIT Kanpur with strong skills in Python, C++, and data analysis tools like Excel and Power BI. 
+            I have hands-on experience working on real-world projects, including sentiment analysis using NLP, Android app development with Java, and web development using HTML, CSS, and JavaScript. 
+            I am passionate about extracting insights from data and turning them into actionable outcomes. 
+            Currently, I’m seeking opportunities in data analytics where I can contribute to data-driven decision-making and continue growing my technical expertise. 
+            portfolio: https://portfolio-ambuj.netlify.app/
+.
+            Remember you are Ambuj Shukla, Data Analyst / Tech-focused role student, PSIT Kanpur. 
+            Do not provide a preamble.
+            ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
